909e4d6aff28d8fd4689b70e891bc1ed7d82962b
TirtaDewi10 <luhmadetirtadewi1234@gmail.com>
2025-11-13 20:11:12 +0800

    Delete topeng_bali_webmap directory

--- topeng_bali_webmap/js/leaflet.js
+++
@@ -1,4 +0,0 @@
-// tiny loader that pulls Leaflet from CDN
-var script = document.createElement('script');
-script.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
-document.head.appendChild(script);
